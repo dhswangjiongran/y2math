877dc1195daebaf12c958df5a8ee0dcbf456daaa
dhswangjiongran <38025643+dhswangjiongran@users.noreply.github.com>
2019-10-22 14:21:33 +0800

    Update t13_mode1.py

--- t13_mode1.py
+++ t13_mode1.py
@@ -2,8 +2,11 @@
 
 import statistics
 
-A = [19, 18, 46, 18, 18, 19]
+A = [19, 18, 46, 18, 18, 19, 18, 19, 18]
 print(statistics.mode(A))
 
 # the above only works if there is only 1 mode
 # what if there is more than 1 mode?
+
+if (statistics.mode(A)>2)
+Print = both.mode(A)
